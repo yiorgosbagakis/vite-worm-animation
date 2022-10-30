babff3d0b475ca0ebe5ce0dccba7786e2ea90d42
Yiorgos <yiorgos.bagakis@protonmail,com>
2022-10-30 07:53:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import { select } from 'd3';
+import { dataMaker } from './dataMaker.js'
+import { vizMaker } from './vizMaker.js'
+import './style.css'
+
+const w = window.innerWidth
+const h = window.innerHeight
+
+const svg = select('body')
+  .append('svg')
+	.attr('width', w)
+	.attr('height', h)
+
+let t = 0
+
+setInterval(() => {
+const mydata = dataMaker(t)
+  
+  svg.call(vizMaker, mydata)
+  t = t + 0.125
+  
+},100)
+
